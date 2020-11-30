e1ef616c1edbdf713ca425d70a17d83ff6e32138
Jimmy Moon <ragingwind@Jimmys-iMac.local>
2020-11-30 16:06:29 +0900

    Update cask

--- Casks/devdogs.rb
+++ Casks/devdogs.rb
@@ -1,9 +1,10 @@
 cask "devdogs" do
   url "https://github.com/ragingwind/devdogs/releases/download/v0.2.1/Devdogs-0.2.1.zip"
-  appcast "https://www.devdocs.com"
+  sha256 "004cb15be88bbef2516d0ed3d75f6e4e198b5a0700ecfeba4131f34cc1cb869f"
+  appcast "https://github.com/ragingwind/devdogs/"
   name "Devdogs"
   desc "Open links in any browser"
   homepage "https://github.com/ragingwind/devdogs/"
 
-  depends_on macos: ">= :yosemite"
+  app "Devdogs.app"
 end
